b9153e6339219b7014f604e3a06c3cdd14d77e12
daniel <donniedev82@gmail.com>
2019-03-13 00:27:41 +0000

    lab 5

--- Lab 5:Reusable Barriers/Barrier.h
+++ Lab 5:Reusable Barriers/Barrier.h
@@ -1,43 +1,43 @@
 /* Barrier.h --- Barrier.h
- * 
+ *
  * Filename: Barrier.h
- * Description: 
+ * Description:
  * Author: Joseph
- * Maintainer: 
+ * Maintainer:
  * Created: Tue Jan  8 12:12:03 2019 (+0000)
- * Version: 
+ * Version:
  * Package-Requires: ()
  * Last-Updated: Tue Jan  8 12:15:54 2019 (+0000)
  *           By: Joseph
  *     Update #: 2
- * URL: 
- * Doc URL: 
- * Keywords: 
- * Compatibility: 
- * 
+ * URL:
+ * Doc URL:
+ * Keywords:
+ * Compatibility:
+ *
  */
 
-/* Commentary: 
- * 
- * 
- * 
+/* Commentary:
+ *
+ *
+ *
  */
 
 /* Change Log:
- * 
- * 
+ *
+ *
  */
 
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -46,10 +46,15 @@
 #pragma once
 class Barrier{
   int numThreads;
+
+private:
+  theMutex = std::make_shared<Semaphore>(1);
+  theSemaphore = std::make_shared<Semaphore>(0);
  public:
   Barrier(int numThreads);
   virtual ~Barrier();
-  void wait();  
+  void wait();
+
 };
 
 
